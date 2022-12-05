2a3fa7a3d491cd6a11544b49f2415410c8840f9b
Igor Cotrim <igor.santos@verity.com.br>
2022-12-05 14:54:51 -0300

    feat: answering json

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/igor-cotrim/go-rest-api/models"
 	"github.com/igor-cotrim/go-rest-api/routes"
 )
 
 func main() {
+	models.Personalities = []models.Personality{
+		{Name: "Nome 1", Story: "Historia 1"},
+		{Name: "Nome 2", Story: "Historia 2"},
+	}
+
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
