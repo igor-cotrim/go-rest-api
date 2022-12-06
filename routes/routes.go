b21656d6585af444f4fd1291f779b57412b0f1c6
Igor Cotrim <igor.santos@verity.com.br>
2022-12-06 10:20:27 -0300

    feat: middleware to add content type

--- routes/routes.go
+++ routes/routes.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/igor-cotrim/go-rest-api/controllers"
+	"github.com/igor-cotrim/go-rest-api/middlewares"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middlewares.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalities", controllers.AllPersonalities).Methods("Get")
 	r.HandleFunc("/api/personalities/{id}", controllers.ReturnsPersonality).Methods("Get")
