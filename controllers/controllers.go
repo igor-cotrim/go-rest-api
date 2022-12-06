e3595f9fe49ddb97827131093675493b1f4fd3bf
Igor Cotrim <igor.santos@verity.com.br>
2022-12-06 09:35:27 -0300

    feat: connect db

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/igor-cotrim/go-rest-api/database"
 	"github.com/igor-cotrim/go-rest-api/models"
 )
 
@@ -15,16 +15,16 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func AllPersonalities(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(models.Personalities)
+	var p []models.Personality
+	database.DB.Find(&p)
+	json.NewEncoder(w).Encode(p)
 }
 
 func ReturnsPersonality(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
+	var personality models.Personality
 
-	for _, personality := range models.Personalities {
-		if strconv.Itoa(personality.Id) == id {
-			json.NewEncoder(w).Encode(personality)
-		}
-	}
+	database.DB.First(&personality, id)
+	json.NewEncoder(w).Encode(personality)
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/igor-cotrim/go-rest-api/database"
 	"github.com/igor-cotrim/go-rest-api/models"
 	"github.com/igor-cotrim/go-rest-api/routes"
 )
@@ -12,6 +13,7 @@ func main() {
 		{Id: 1, Name: "Nome 1", Story: "Historia 1"},
 		{Id: 2, Name: "Nome 2", Story: "Historia 2"},
 	}
+	database.ConnectToDatabase()
 
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
